166c4e7837662a074c89da64338d793e6c6febdb
Armagetron <armagetron@live.de>
2018-07-02 13:45:43 +0200

    reformat code

--- generate_data.py
+++ generate_data.py
@@ -1,9 +1,10 @@
 import random
 
+
 # Generates random integers:
 #	n - number of integers generated
 #	lowerBound - inclusive lower bound 
 # 	upperBound - inclusive upper bound
 
 def generateData(n, lowerBound, upperBound):
-	return [random.randint(lowerBound, upperBound) for _ in range(n)]
+    return [random.randint(lowerBound, upperBound) for _ in range(n)]
